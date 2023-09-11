59e92e7a5f6caf5bdde882ad757cf3530821c0d6
qianyiii <B210230C@sc.edu.my>
2023-09-11 11:32:18 +0800

    Initial commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.q4 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.q4 to javafx.fxml;
     exports com.example.q4;
 }
